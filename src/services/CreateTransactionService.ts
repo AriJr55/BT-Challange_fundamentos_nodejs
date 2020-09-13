9ab1a81812b5729a50f9da3010b69d0f426f60d2
Ari Junior <ari_jr12@yahoo.com.br>
2020-09-12 21:10:41 -0300

    Desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Trans {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Trans): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    if (type !== 'income' && type !== 'outcome') {
+      throw new Error('Operação inválida');
+    }
+    if (balance.total < value && type === 'outcome') {
+      throw new Error('Saldo infuciente');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
